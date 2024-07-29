aff8f32cb2026403445efdc2e4ccea6aa9f5a0ff
rasakii <raskeemono75@hotmail.com>
2024-07-29 16:50:39 +0100

    Enhanced Cross browser testing

---
+++ CollaboratedHRM/Hook/Hook.cs
@@ -0,0 +1,25 @@
+﻿using CollaboratedHRM.Setup;
+using TechTalk.SpecFlow;
+
+namespace CollaboratedHRM.Hook
+{
+    [Binding]
+    internal class Hook
+    {
+        BasePage _basepage;
+        public Hook(BasePage basepage)
+        {
+            _basepage = basepage;   
+        }
+        [BeforeScenario]
+        public void BeforeScenario() 
+        { 
+            _basepage.InitializeBrowser("firefox");
+        }
+        [AfterScenario]
+        public void AfterScenario() 
+        {
+            _basepage.CloseApplicationUnderTest();
+        }
+    }
+}
---
+++ CollaboratedHRM/Setup/BasePage.cs
@@ -0,0 +1,67 @@
+﻿using BoDi;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace CollaboratedHRM.Setup
+{
+   
+    public class BasePage
+    {
+        private readonly IObjectContainer _objectContainer;
+        public IWebDriver _driver;
+        string baseUrl = "https://opensource-demo.orangehrmlive.com/";
+        
+        public BasePage(IObjectContainer objectContainer) 
+        {
+            _objectContainer = objectContainer;
+
+        }
+
+        //public Base(IObjectContainer objectContainer)
+        //{
+        //    _objectContainer = objectContainer;
+        //    ChromeOptions options = new ChromeOptions();
+        //    _driver = new ChromeDriver(options);
+        //    _objectContainer.RegisterInstanceAs<IWebDriver>(_driver);
+        //}
+        public void InitializeBrowser(string browser)
+        {
+          
+            switch (browser.ToLower())
+            {
+                case "firefox":
+                    _driver = new FirefoxDriver();
+                    break;
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    _driver = new ChromeDriver(chromeOptions);
+                    break;
+                case "edge":
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    _driver = new EdgeDriver(edgeOptions);
+                    break;
+                default:
+                    throw new ArgumentException("Unsupported browser: " + browser);
+            }
+            _objectContainer.RegisterInstanceAs(_driver);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+          
+        }
+
+        public void LoadApplicationUnderTest()
+        {
+       
+            _driver.Navigate().GoToUrl(baseUrl);
+            _driver.Manage().Window.Maximize();
+
+        }
+
+        public void CloseApplicationUnderTest()
+        {
+            _driver?.Quit();
+        }
+    }
+}
